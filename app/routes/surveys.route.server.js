4be8da275a0220e75cee1379a74a0a4a4238c05b
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-11-29 18:44:52 +0300

    Added another survey template to the list page

--- app/routes/surveys.route.server.js
+++ app/routes/surveys.route.server.js
@@ -14,8 +14,8 @@ const router = Router();
 router.get('/survey-list', DisplaySurveyList);
 router.get('/survey-add', AuthGuard, DisplaySurveyAddPage);
 router.post('/survey-add', AuthGuard,ProcessSurveyAddPage);
-router.post('/survey-edit/:id', AuthGuard,ProcessSurveyEditPage);
-router.get('/survey-edit/:id', AuthGuard,DisplaySurveyEditPage);
+router.post('/survey-edit/:id',ProcessSurveyEditPage);
+router.get('/survey-edit/:id',DisplaySurveyEditPage);
 router.get('/survey-delete/:id', AuthGuard,ProcessSurveyDelete);
 
 export default router;
